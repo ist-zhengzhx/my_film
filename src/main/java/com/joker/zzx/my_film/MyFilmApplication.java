b4dbcdd56d863e58582c01a14053853cd64beef0
joker <441759389@163.com>
2019-11-13 18:43:27 +0800

    工程初始化

---
+++ src/main/java/com/joker/zzx/my_film/MyFilmApplication.java
@@ -0,0 +1,13 @@
+package com.joker.zzx.my_film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joker/zzx/my_film/MyFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joker.zzx.my_film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
